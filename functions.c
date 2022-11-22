48af879181774bb27210f6bd8494566c9946505f
TosBam <samuelson.os19@gmail.com>
2022-11-22 15:11:51 -0800

    the first function file

--- functions.c
+++ functions.c
@@ -19,7 +19,9 @@ int print_char(va_list types, char buffer[],
 
 	return (handle_write_char(c, buffer, flags, width, precision, size));
 }
+
 /************************* PRINT A STRING *************************/
+
 /**
  * print_string - Prints a string
  * @types: List a of arguments
@@ -74,7 +76,9 @@ int print_string(va_list types, char buffer[],
 
 	return (write(1, str, length));
 }
+
 /************************* PRINT PERCENT SIGN *************************/
+
 /**
  * print_percent - Prints a percent sign
  * @types: Lista of arguments
